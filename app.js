c74a739c6e4a532c6a0d40006e9078f65e78bd37
Fisher <truefisherking@gmail.com>
2018-01-02 13:06:35 -0500

    added ejs with passed var

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var express = require("express");
 var app = express();
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:word", function(req, res){
+    var word = req.params.word;
+    res.render("love.ejs", {thingWord: word});
+});
+
+app.listen(3000, function(){
+    console.log("Server Started");
+});
